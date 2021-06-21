4a389e68e1f4362f0de5ce67324c5ae3dd2127bb
Joel Gage <jrossgage@gmail.com>
2021-06-21 15:40:10 -0500

    On the challenge exercise

---
+++ Repositories/WalkerRepository.cs
@@ -0,0 +1,113 @@
+﻿using DogGo.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+
+namespace DogGo.Repositories
+{
+    public class WalkerRepository : IWalkerRepository
+    {
+        private readonly IConfiguration _config; 
+
+        public WalkerRepository(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+
+        public List<Walker> GetAllWalkers()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                                        SELECT w.Id, w.[Name], w.ImageUrl, w.NeighborhoodId, nh.[Name] as NHName
+                                        FROM Walker w
+                                        join Neighborhood nh on w.NeighborhoodId = nh.id;";
+                                        
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    List<Walker> walkers = new List<Walker>();
+                    while (reader.Read())
+                    {
+
+                        Walker walker = new Walker
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Name = reader.GetString(reader.GetOrdinal("Name")),
+                            ImageUrl = reader.GetString(reader.GetOrdinal("ImageUrl")),
+                            NeighborhoodId = reader.GetInt32(reader.GetOrdinal("NeighborhoodId"))
+                        };
+
+                        Neighborhood neighborhood = new Neighborhood
+                        {
+                            Id = walker.NeighborhoodId,
+                            Name = reader.GetString(reader.GetOrdinal("NHName"))
+                        };
+                        walker.Neighborhood = neighborhood;
+                        walkers.Add(walker);
+                    }
+
+                    reader.Close();
+
+                    return walkers;
+                }
+            }
+        }
+
+        public Walker GetWalkerById(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                                        SELECT w.Id, w.[Name], w.ImageUrl, w.NeighborhoodId, nh.[Name] as NHName
+                                        FROM Walker w
+                                        join Neighborhood nh on w.NeighborhoodId = nh.id
+                                        WHERE w.Id = @id";
+
+                    cmd.Parameters.AddWithValue("@id", id);
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        Walker walker = new Walker
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Name = reader.GetString(reader.GetOrdinal("Name")),
+                            ImageUrl = reader.GetString(reader.GetOrdinal("ImageUrl")),
+                            NeighborhoodId = reader.GetInt32(reader.GetOrdinal("NeighborhoodId"))
+                        };
+                        Neighborhood neighborhood = new Neighborhood
+                        {
+                            Id = walker.NeighborhoodId,
+                            Name = reader.GetString(reader.GetOrdinal("NHName"))
+                        };
+                        walker.Neighborhood = neighborhood;
+
+                        reader.Close();
+                        return walker;
+                    }
+                    else
+                    {
+                        reader.Close();
+                        return null;
+                    }
+                }
+            }
+        }
+    }
+}
